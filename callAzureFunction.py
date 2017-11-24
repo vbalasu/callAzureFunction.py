f332d1bc6413de58ba74e6ade6f03a836aaa2410
vbalasu <vbalasu@gmail.com>
2017-11-24 08:03:21 -0500

    Add files via upload

---
+++ callAzureFunction.py
@@ -0,0 +1,12 @@
+def callAzureFunction(baseurl, code, clientId, json):
+    import requests
+    querystring = {"code":code,"clientId":clientId}
+    payload = json
+    headers = {
+        'content-type': "application/json",
+        'cache-control': "no-cache",
+        }
+    response = requests.request("POST", baseurl, data=payload, headers=headers, params=querystring)
+    return(response.text)
+
+print(callAzureFunction("https://cloudmaticafunctions.azurewebsites.net/api/echo", "JPPXiWIHCmGciaQ6Kj4qpmayDkXcIBFBi68GXziYkMMeWAUMIZ388Q==","default", "{\"text\":\"Python Postman\"}"))
